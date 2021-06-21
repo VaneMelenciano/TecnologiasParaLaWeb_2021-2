5185ef4fc8c40b85b3105541169c7a847a18eccf
Vanessa Melenciano Llamas <54332038+VaneMelenciano@users.noreply.github.com>
2021-06-21 13:19:32 -0500

    Create ejercicio-10.js

---
+++ Departamenta 3/js/ejercicio-10.js
@@ -0,0 +1,33 @@
+function mostrarDefault(){
+    //alert("def");
+    $('#div-default').toggle(500);
+}
+function mostrarPrimary(){
+    $('#div-primary').toggle(500);
+}
+function mostrarSecundary(){
+    $('#div-secundary').toggle(500);
+}
+function mostrarSuccess(){
+    $('#div-success').toggle(500);
+}
+function mostrarInfo(){
+    //alert("info");
+    $('#div-info').toggle(500);
+}
+function mostrarDanger(){
+    $('#div-danger').toggle(500);
+}
+function mostrarWarning(){
+    //alert("warining");
+    $('#div-warning').toggle(500);
+}
+
+
+
+
+
+
+
+
+
